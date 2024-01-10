9cd380af919086277fc369fcf4bab6196611ade1
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-01-10 12:12:16 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,6 @@
+function solution(my_string, overwrite_string, s) {
+    let str = [];
+    str = my_string.split("")
+    str.splice(s, overwrite_string.length, overwrite_string)
+    return str.join("")
+}
