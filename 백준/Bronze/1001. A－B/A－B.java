df6ac4ca1553cd061b9124cc09678d55f4ba9619
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-04-11 21:57:52 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //1. 두 정수 A와 B를 입력한다.
         //2. A-B를 출력해라
-
+        //3. 야호
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
