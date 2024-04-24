2702db49130d7d0d65071e4536d3bd89ac3a1397
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-04-24 17:36:31 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 188 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //1. 고려대학교를 출력해라
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = "고려대학교";
+
+        System.out.println(a);
+
+    }
+}
