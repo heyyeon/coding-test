7f2ee4e71a625614b2ac49ef21020a650106420f
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-04-29 19:46:06 +0900

    [Bronze V] Title: 연세대학교, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+
+        if(N == 0){
+            System.out.println("YONSEI");
+        }else if(N == 1){
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
