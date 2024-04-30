c8b1003d5e210dee798cef4b11f0a183d410ebb0
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-04-30 18:38:12 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String A = "강한친구 ";
+        String B = "대한육군";
+
+        System.out.println(A + B);
+        System.out.println(A + B);
+
+    }
+}
