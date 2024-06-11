b62e63ca6b987709f270e312adb0690e0c915a1b
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-06-11 19:31:55 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,26 +2,30 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Exception {
     public static void main(String[] args) throws IOException {
-        //1. 첫째줄에 정수 개수 n개
-        //2. 둘째 줄에는 정수가 공백으로 구분되어져있다.
-        //3. 셋째 줄에는 찾으려는 정수 v가 주어진다.
-        //4. 주어진 정수 n개의 정수 중 v가 몇개인지 출력해라
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); //정수의 개수
-        int[] arr = new int[n]; // n의 배열
+
+        //1. 정수의 개수 : n
+        //2. 정수들
+        //3. 찾으려는 정수 v
+        // : 정수 중 v가 몇개인지 출력해라
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        int count = 0;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt(); //배열도 입력받는다.
-            }
-        int v = sc.nextInt();//정수를 입력받는다.
-        int cnt = 0; // 출력할 값
-
-        for(int a : arr){ //for each 문 활용
-            if (a == v) cnt++;
+            arr[i] = sc.nextInt();
         }
-        System.out.println(cnt);
+        int v = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == v) {
+                count ++;
+            }
+        }
+        System.out.println(count);
     }
 }
