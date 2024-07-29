e1acd40fd58c261f5b589da71ebd01c036e2fb6b
heyyeon <131832573+heyyeon@users.noreply.github.com>
2024-07-29 23:57:39 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String a = sc.next();
+        int score = 0;
+
+        if(a.equals("miss")) {
+            score = 0;
+        }else if(a.equals("bad")) {
+            score = n * 200;
+        }else if(a.equals("cool")) {
+            score = n * 400;
+        }else if(a.equals("great")) {
+            score = n * 600;
+        }else if(a.equals("perfect")) {
+            score = n * 1000;
+        }
+
+        System.out.println(score);
+    }
+}
